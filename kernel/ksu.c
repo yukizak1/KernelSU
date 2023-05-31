2613c053907e6c0a5a95cfac1f09a92492e41fbd
Yukizaki Miyamura <96718991+yukizak1@users.noreply.github.com>
2023-05-31 15:47:41 +0700

    ksu: disable ksu calls using kprobe

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -30,9 +30,6 @@ int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 					    flags);
 }
 
-extern void ksu_enable_sucompat();
-extern void ksu_enable_ksud();
-
 int __init kernelsu_init(void)
 {
 #ifdef CONFIG_KSU_DEBUG
@@ -52,13 +49,6 @@ int __init kernelsu_init(void)
 	ksu_allowlist_init();
 
 	ksu_uid_observer_init();
-
-#ifdef CONFIG_KPROBES
-	ksu_enable_sucompat();
-	ksu_enable_ksud();
-#else
-	pr_alert("KPROBES is disabled, KernelSU may not work, please check https://kernelsu.org/guide/how-to-integrate-for-non-gki.html");
-#endif
 
 	return 0;
 }
